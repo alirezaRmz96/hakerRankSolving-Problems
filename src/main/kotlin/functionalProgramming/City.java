e90007f9fb75f4c180688ced4cd640ffec7b78d7
alireza.ramezani <alireza.ramezani@partsoftware.com>
2023-04-04 22:53:22 +0330

    The sevenTeen day

---
+++ src/main/kotlin/functionalProgramming/City.java
@@ -0,0 +1,24 @@
+package functionalProgramming;
+
+public class City {
+    private double longitude;
+    private double latitude;
+
+    public double getLogitude() {
+        return longitude;
+    }
+
+    public void setLogitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+
+}
